8fbb7d72c5855cc67af6b1f7d3d9f818113a510f
abhishek-girnar <abhishek.singh1@girnarsoft.co.in>
2023-04-07 02:16:33 +0530

    testing setup done

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,9 +5,34 @@
 import 'react-native';
 import React from 'react';
 import HomeScreen from '../src/screens/Home/HomeScreen';
-import { render} from "@testing-library/react-native";
+import UserListScreen from '../src/screens/UserList/UserListScreen';
+import { render } from "@testing-library/react-native";
+import { MockedProvider } from '@apollo/react-testing';
+import * as Queries from "../src/graphql/listCustomersQuery";
+jest.useFakeTimers()
 
+const mocks = [
+  {
+    request: {
+      query: Queries.ZELLER_LIST_CUSTOMER_QUERY
+    }
+  },
+];
+
+let props: any;
+const createTestProps = (props: Object) => ({
+  navigation: {
+    navigate: jest.fn()
+  },
+  ...props
+});
 
 it('renders correctly', () => {
-  render(<HomeScreen />);
+
+  props = createTestProps({});
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <UserListScreen {...props} />
+    </MockedProvider>
+  );
 });
