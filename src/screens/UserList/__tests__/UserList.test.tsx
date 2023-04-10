df3ce6af7558240041a8b30a8d0481bb595f4fbd
abhishek-girnar <abhishek.singh1@girnarsoft.co.in>
2023-04-11 02:27:36 +0530

    unit test done

--- src/screens/UserList/__tests__/UserList.test.tsx
+++ src/screens/UserList/__tests__/UserList.test.tsx
@@ -1,22 +1,38 @@
 import 'react-native';
-import React, { useState } from 'react';
+import React from 'react';
 import UserListScreen from '../UserListScreen';
 import UserType from '../UserType';
 import UserList from '../UserList';
-import { fireEvent, render, act, waitFor, renderHook } from "@testing-library/react-native";
+import { fireEvent, render, act, waitFor } from "@testing-library/react-native";
 import { MockedProvider } from '@apollo/react-testing';
 import * as Queries from "../../../graphql/listCustomersQuery";
 import { strings } from "../../../utils";
-import { getUserTypeById, USER_TYPES } from '../UserTypeArray';
-import { CustomerListProps } from '../../../types';
-import renderer from 'react-test-renderer';
+import { getUserTypeById } from '../UserTypeArray';
 
 jest.useFakeTimers()
-let list = [{
+const list = [{
     id: "1",
     name: "Abhishek",
     email: "abhishek@gmail.com",
     role: "Manager"
+},
+{
+    id: "4",
+    name: "Abhishek2",
+    email: "abhishek2@gmail.com",
+    role: "Manager"
+},
+{
+    id: "2",
+    name: "Zeller",
+    email: "Zeller@gmail.com",
+    role: "Admin"
+},
+{
+    id: "2",
+    name: "Zeller2",
+    email: "Zeller2@gmail.com",
+    role: "Admin"
 }]
 
 
@@ -24,6 +40,13 @@ const mocks = [
     {
         request: {
             query: Queries.ZELLER_LIST_CUSTOMER_QUERY
+        },
+        result: {
+            data: {
+                listZellerCustomers: {
+                    items: list
+                }
+            }
         }
     },
 ];
@@ -31,9 +54,10 @@ const mocks = [
 const changeSelectedType = jest.fn()
 
 let props: any;
+const navigate = jest.fn()
 const createTestProps = (props: Object) => ({
     navigation: {
-        navigate: jest.fn()
+        navigate: navigate
     },
     changeSelectedType: changeSelectedType,
     ...props
@@ -58,7 +82,7 @@ it('renders all default elements', () => {
 
 it('test click on user type radio button', async () => {
     props = createTestProps({});
-    const { getByTestId, getAllByText } = render(
+    const { getByTestId, getAllByText, getAllByTestId } = render(
         <MockedProvider mocks={mocks} addTypename={false}>
             <UserListScreen {...props} >
                 <UserType {...props} />
@@ -67,13 +91,59 @@ it('test click on user type radio button', async () => {
     );
     fireEvent.press(getByTestId(strings.USER_TYPE_ITEM_TEST_ID + "_" + getUserTypeById(1)?.label))
     expect(getAllByText(getUserTypeById(1)?.label + " " + strings.Users).length).toBe(1)
+
+    await act(async () => {
+        await waitFor(() => {
+            expect(getAllByTestId(strings.USER_LIST_ITEM_ID + "_Zeller").length).toBeGreaterThan(0)
+        })
+    })
+});
+
+
+it('test search box', async () => {
+    props = createTestProps({});
+    const { getByTestId, getAllByTestId } = render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+            <UserListScreen {...props} >
+                <UserType {...props} />
+            </UserListScreen>
+        </MockedProvider>
+    );
+
+
+    await act(async () => {
+        await waitFor(() => {
+            fireEvent(getByTestId(strings.SEARCH_BOX_TEST_ID), "onChangeText", "Abhishek2")
+            expect(getAllByTestId(strings.USER_LIST_ITEM_ID + "_Abhishek2").length).toEqual(1)
+        })
+    })
+});
+
+
+it('test navigation on click on user from users list', async () => {
+    props = createTestProps({});
+    const { getByTestId } = render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+            <UserListScreen {...props} >
+                <UserType {...props} />
+            </UserListScreen>
+        </MockedProvider>
+    );
+
+
+    await act(async () => {
+        await waitFor(() => {
+            fireEvent.press(getByTestId(strings.USER_LIST_ITEM_ID + "_Abhishek"))
+            expect(navigate).toBeCalledWith("HomeScreen")
+        })
+    })
 });
 
 
 test('test click on user from users list', () => {
     props = createTestProps({});
     const onUserClick = jest.fn()
-    const { getByTestId, getAllByText, getAllByTestId } = render(
+    const { getByTestId } = render(
         <UserList {...props} onUserClick={onUserClick} list={list} selectedType={0} itemTestId={strings.USER_LIST_ITEM_ID} />
     );
     fireEvent.press(getByTestId(strings.USER_LIST_ITEM_ID + "_Abhishek"))
