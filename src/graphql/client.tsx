1580df50780e000dffc7ba01f7b4a51fd53cdf78
abhishek-girnar <abhishek.singh1@girnarsoft.co.in>
2023-04-11 04:45:12 +0530

    stored keys in variables

--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -1,12 +1,15 @@
 import fetch from 'cross-fetch';
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+
+// TODO:store keys in secured storage if sensitive data
 const url = "https://prrwjjssnvhpbcdwbcwx3nm3zm.appsync-api.ap-southeast-2.amazonaws.com/graphql"
+const api_key = "da2-psmv7fcrw5dlpmp5orner2xf7i"
 
 const client = new ApolloClient({
   link: new HttpLink({
     uri: url, headers: {
-      "x-api-key": "da2-psmv7fcrw5dlpmp5orner2xf7i"
+      "x-api-key": api_key
     }, fetch
   }),
   cache: new InMemoryCache()
